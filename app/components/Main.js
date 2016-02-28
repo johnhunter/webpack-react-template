0ae6a2998fd54ad2ddea9090fec98a5749570bba
John Hunter <john@johnhunter.info>
2016-02-28 11:43:27 +0000

    Initial commit

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Main = React.createClass({
+    render: () => {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<Main />, document.getElementById('app'));

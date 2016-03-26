7821dfa7bf47b0c0aaa632210d3bb62a36c69cce
John Hunter <john@johnhunter.info>
2016-03-26 23:36:37 +0000

    Add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,37 @@
+var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './app/components/Main.js',
+  entry: [
+  'webpack-dev-server/client?http://localhost:8080',
+  'webpack/hot/only-dev-server',
+  './app/components/Main.js'
+  ],
   output: {
     filename: 'public/bundle.js'
+  },
+  devServer: {
+    contentBase: './public',
+    hot: true
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /(node_modules|bower_components)/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015']
-      }
+      loader: 'babel'
     },{
       test: /\.scss$/,
       loader: ExtractTextPlugin.extract(['css','sass'])
     }]
   },
   plugins: [
-      new ExtractTextPlugin('public/style.css', {
-        allChunks: true
-      })
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('public/style.css', {
+      allChunks: true
+    })
   ]
 };
+
